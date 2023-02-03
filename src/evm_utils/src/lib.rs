4542c8e21b56301a16da88606561024edd075108
Stopak <1740874+stopak@users.noreply.github.com>
2023-02-03 10:58:44 +0100

    Updated candid file, readme in progress

--- src/evm_utils/src/lib.rs
+++ src/evm_utils/src/lib.rs
@@ -1,6 +1,35 @@
+use candid::export_service;
+use ic_cdk::query;
+
 mod hash;
 mod rlp;
 mod transaction;
 mod tree;
 mod types;
 mod utils;
+
+use crate::types::num::U256;
+use crate::types::rlp::List;
+use crate::types::transaction::Transaction;
+
+#[query(name = "__get_candid_interface_tmp_hack")]
+fn export_candid() -> String {
+    export_service!();
+    __export_service()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn save_candid() {
+        use std::env;
+        use std::fs::write;
+        use std::path::PathBuf;
+
+        let dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+        let dir = dir.parent().unwrap().parent().unwrap().join("candid");
+        write(dir.join("utils.did"), export_candid()).expect("Write failed.");
+    }
+}
--- src/evm_utils/src/transaction.rs
+++ src/evm_utils/src/transaction.rs
@@ -1,8 +1,6 @@
-use candid::{candid_method, export_service};
+use candid::candid_method;
 use ic_cdk_macros::query;
 
-use crate::types::num::U256;
-use crate::types::rlp::List;
 use crate::types::transaction::Transaction;
 
 /// Encodes transaction in rlp, ready to be signed
@@ -22,26 +20,4 @@ fn parse_transaction(data: Vec<u8>) -> Result<Transaction, String> {
         Transaction::decode(&data).map_err(|x| format!("Error while decoding transaction {x}"))?;
 
     Ok(item)
-}
-
-#[query(name = "__get_candid_interface_tmp_hack")]
-fn export_candid() -> String {
-    export_service!();
-    __export_service()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn save_candid() {
-        use std::env;
-        use std::fs::write;
-        use std::path::PathBuf;
-
-        let dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-        let dir = dir.parent().unwrap().parent().unwrap().join("candid");
-        write(dir.join("utils.did"), export_candid()).expect("Write failed.");
-    }
 }
