72ac8ed8365d88e85c8026e7d63ef5151903d882
stopak <1740874+stopak@users.noreply.github.com>
2023-05-12 12:59:16 +0200

    Updated tests to work with github workflows

--- __tests__/_common.mjs
+++ __tests__/_common.mjs
@@ -14,45 +14,14 @@ if (!globalThis.fetch) {
 export async function initCanisters() {
     if (context !== undefined) return;
     context = new TestContext();
-    let id = getCanisterId(false, "evm_utils");
-    await context.deploy(".dfx/local/canisters/evm_utils/evm_utils.wasm", {
-        id: Principal.from(id),
-        candid: ".dfx/local/canisters/evm_utils/evm_utils.did"
-    })
-}
-
-
-export function getCanisterId(useProd, canister) {
-    let canisterId = null;
-
-    if (useProd) {
-        var data = JSON.parse(fs.readFileSync("../canister_ids.json"))
-        canisterId = data[canister]["ic"];
-
-    } else {
-        var data = JSON.parse(fs.readFileSync(".dfx/local/canister_ids.json"))
-        canisterId = data[canister]["local"];
-    }
-
-    console.log(canister + " Canister Id: " + canisterId);
-    return canisterId;
-}
-
-export async function getIdlFactory(canisterName) {
-    var path = "../.dfx/local/canisters/"+canisterName+"/"+canisterName+".did.js";
-    let { idlFactory } = await import(path);
-
-    return idlFactory;
+    return await context.deploy("target/wasm32-unknown-unknown/release/evm_utils.wasm")
 }
 
 //Returns actor for token canister
 export async function getActor(useProd, canisterName) {
-    await initCanisters();
+    let canister = await initCanisters();
 
-
-    // let httpAgent = null;
-    let canisterId = getCanisterId(useProd, canisterName);
-    let actor = context.getAgent(Principal.anonymous()).getActor(canisterId);
+    let actor = context.getAgent(Principal.anonymous()).getActor(canister);
 
     return actor;
 }
